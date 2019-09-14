8d0e56bda2a193be95e926c4f2e9bdce62eeb0ba
probably-not-porter <pelibby16@earlham.edu>
2019-09-14 11:20:06 -0400

    + topo and sat view, refactor

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+var mapstate = 0;
+require([
+    "esri/Map",
+    "esri/views/MapView"
+], function(Map, MapView) {
+    var map1 = new Map({
+        basemap: "topo"
+    });
+    var map2 = new Map({
+        basemap: "satellite"
+    });
+    var view = new MapView({
+        container: "viewDiv",
+        map: map1,
+        center: [-118.71511,34.09042],
+        zoom: 11
+    });
+    document.querySelector(".btn_topo").addEventListener("click", function(event) {
+        if (mapstate != 0){
+            mapstate = 0;
+            view.map = map1;
+            console.log('switch to topo');
+        }
+        else {
+            console.log('already in state 0');
+        }
+    });
+    document.querySelector(".btn_sat").addEventListener("click", function(event) {
+        if (mapstate != 1){
+            mapstate = 1;
+            view.map = map2;
+            console.log('switch to sat');
+        }
+        else {
+            console.log('already in state 1');
+        }
+    });
+});
