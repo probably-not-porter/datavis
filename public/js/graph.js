4db2cc36642cc6f4727002ff907093174f12cd4a
probably-not-porter <pelibby16@earlham.edu>
2019-10-29 14:34:41 -0400

    split up js

---
+++ public/js/graph.js
@@ -0,0 +1,80 @@
+/*
+#
+# Datavis 2.0
+# Porter Libby - 2019 - initial setup
+# pelibby16@earlham.edu
+#
+# Graph functions
+*/
+
+var lineChart;
+$( document ).ready(function() {
+    lineChart = new Chart(document.getElementById("line-chart"), {
+        type: 'scatter',
+        data: {},
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            title: {
+                display: true,
+                text: 'No Data Selected'
+            }
+        }
+    });
+    console.log( "ready!" );
+});
+
+function createGraph(dataset, title){
+    label_arr = [];
+    elevation_arr = [];
+    times_arr = [];
+    bucket = -1;
+    for (x=0;x<dataset.length;x++){
+        console.log(dataset[x].platformid)
+        if ((x == 0) || (dataset[x-1].platformid != dataset[x].platformid)){
+            bucket++;
+            label_arr.push(dataset[x].platformid);
+            elevation_arr.push([]);
+            times_arr.push([]);
+            console.log(times_arr);
+            console.log(elevation_arr);
+            console.log(label_arr);
+        }
+        if (!(times_arr[bucket].includes(dataset[x].recordtime.split('T')[1]))){
+            newtime = new Date(dataset[x].recordtime).getTime()
+            elevation_arr[bucket].push({x: newtime ,y: (dataset[x].elevation)});
+            times_arr[bucket].push(dataset[x].recordtime.split('T')[1].substring(0,8));
+        }
+    }
+    //elevation_arr = [...new Set(elevation_arr)];
+    //times_arr = [...new Set(times_arr)];
+    console.log('UNIQUE DATA')
+    console.log(elevation_arr);
+    console.log(times_arr);
+    addData(lineChart, times_arr, elevation_arr,label_arr);
+}
+function addData(chart, label_arr, data, title_arr) {
+    console.warn('UPDATING CHART');
+    chart.options.title.text = 'test';
+    for (x=0; x< label_arr.length; x++){
+        chart.data.labels = chart.data.labels.concat(label_arr[x]);
+        var newDataset = {
+            label: title_arr[x],
+            borderColor: getRandomColor(),
+            borderWidth: 3,
+            fill: false,
+            data: data[x],
+        }
+        chart.data.datasets.push(newDataset);
+    }
+    chart.update();
+    console.log(chart);
+}
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
---
+++ public/js/map.js
@@ -0,0 +1,92 @@
+/*
+#
+# Datavis 2.0
+# Porter Libby - 2019 - initial setup
+# pelibby16@earlham.edu
+#
+# Map functions
+*/
+
+var mapstate = 0; // keep track of which map overlay is being used
+var default_center = [-13.7055,65.2941]; //default starting coords for the map view
+var view;
+var map;
+var prev_points = [];
+
+
+require(["esri/Map", "esri/views/SceneView", "esri/views/MapView", "esri/Graphic", "esri/widgets/BasemapToggle", "esri/widgets/CoordinateConversion", "esri/PopupTemplate" ], function(
+    Map,
+    SceneView,
+    MapView,
+    Graphic,
+    BasemapToggle,
+    CoordinateConversion,
+    PopupTemplate
+  ) {
+    map = new Map({
+      basemap: "topo"
+    });
+
+    view = new MapView({
+      center: [-13.68, 65.29],
+      container: "viewDiv",
+      map: map,
+      zoom: 11
+    });
+    var basemapToggle = new BasemapToggle({
+        viewModel: {  // autocasts as new BasemapToggleViewModel()
+            view: view,  // The view that provides access to the map's "streets" basemap
+            nextBasemap: "satellite"  // Allows for toggling to the "hybrid" basemap
+        }
+    });
+    var coordinateConversionWidget = new CoordinateConversion({
+        view: view
+    });
+    view.ui.add(basemapToggle, {
+        position: "bottom-left",
+        width: 200
+    });
+    view.ui.add(coordinateConversionWidget, "bottom-left");
+});
+
+function createPoints(points){
+    require(["esri/Map", "esri/views/SceneView", "esri/views/MapView", "esri/Graphic", "esri/widgets/BasemapToggle", "esri/widgets/CoordinateConversion", "esri/PopupTemplate" ], function(
+    Map,
+    SceneView,
+    MapView,
+    Graphic,
+    BasemapToggle,
+    CoordinateConversion,
+    PopupTemplate
+    ){
+        if (prev_points.length != 0){
+            view.graphics.removeMany(prev_points);
+            prev_points = [];
+        }
+        out_points = [];
+
+        var markerSymbol = {
+            type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+            color: getRandomColor(),
+        };
+
+        for(x=0;x<points.length;x++){
+            var point = {
+                type: "point", // autocasts as new Point()
+                longitude: points[x].longitude,
+                latitude: points[x].latitude
+            };
+            
+            // Create a graphic and add the geometry and symbol to it
+            var pointGraphic = new Graphic({
+                geometry: point,
+                symbol: markerSymbol
+            });
+            
+            out_points.push(pointGraphic);
+        }
+        prev_points = prev_points.concat(out_points);
+        view.graphics.addMany(out_points);
+        console.log('points added to map');
+    });
+}
