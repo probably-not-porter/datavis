b4736054690b9ef61ef77b69716e4f4fe46e4e68
probably-not-porter <pelibby16@earlham.edu>
2019-11-25 14:06:12 -0500

    add graph comments

--- public/js/graph.js
+++ public/js/graph.js
@@ -8,9 +8,11 @@
 */
 
 var lineChart;
+
+// create null chart for no data case
 $( document ).ready(function() {
     lineChart = new Chart(document.getElementById("line-chart"), {
-        type: 'scatter',
+        type: 'scatter', // init to scatter plot
         data: {},
         options: {
             responsive: true,
@@ -24,50 +26,48 @@ $( document ).ready(function() {
 });
 
 function createGraph(dataset, title,color){
-    times_arr = [];
-    data = [[]];
-    loc = 0;
-    types = [];
+    times_arr = []; // x axis ticks (timestamps)
+    data = [[]]; // this array will each set of data (split by sensor)
+    loc = 0; // set tracking
+    types = []; // type tracking
 
+    // sort dataset according to sensortype value
+    dataset.sort(function(a, b){
+        var nameA=a.sensortype, nameB=b.sensortype;
+        if (nameA < nameB) //sort string ascending
+            return -1 
+        if (nameA > nameB)
+            return 1
+        return 0 //default return value (no sorting)
+    })
+
+    // create independent buckets of data for each plot
     for (x=0;x<dataset.length;x++){
-        
         if (!(times_arr.includes(moment(dataset[x].recordtime)))){
             times_arr.push(moment(dataset[x].recordtime));
-            //data[0].push({x:moment(dataset[x].recordtime),y:dataset[x].elevation});
         }
-
-        dataset.sort(function(a, b){
-            var nameA=a.sensortype, nameB=b.sensortype;
-            if (nameA < nameB) //sort string ascending
-                return -1 
-            if (nameA > nameB)
-                return 1
-            return 0 //default return value (no sorting)
-        })
-        if (x == 0){
-            types.push(dataset[x].sensortype + ' (' + dataset[x].sensorunits + ')');
+        if (x == 0){ // if first element
+            types.push(dataset[x].sensortype + ' (' + dataset[x].sensorunits + ')'); // these will be labels
             data[loc].push({x:moment(dataset[x].recordtime), y:dataset[x].value_1,sensorid:dataset[x].sensorid, sensorunits:dataset[x].sensorunits});
-        }else if ((data[data.length - 1][0]) && (data[data.length - 1][0].sensorid == dataset[x].sensorid)){
+        }else if ((data[data.length - 1][0]) && (data[data.length - 1][0].sensorid == dataset[x].sensorid)){ // data matches last used bucket
             data[loc].push({x:moment(dataset[x].recordtime), y:dataset[x].value_1, sensorid:dataset[x].sensorid, sensorunits:dataset[x].sensorunits});
         }
-        else{
-            data.push([]);
+        else{ // no match, create new bucket
+            data.push([]); // create empty subset
             loc++;
-            types.push(dataset[x].sensortype + ' (' + dataset[x].sensorunits + ')');
+            types.push(dataset[x].sensortype + ' (' + dataset[x].sensorunits + ')'); // these will be labels
             data[loc].push({x:moment(dataset[x].recordtime), y:dataset[x].value_1, sensorid:dataset[x].sensorid, sensorunits:dataset[x].sensorunits});
         }
     }
-    console.log(data);
     addData(lineChart, times_arr, data,types,color);
 }
 function addData(chart,times,data,types,color) {
     console.warn('UPDATING CHART: this might take a minute!');
-    console.log(types);
     
     if (chart){
-        chart.destroy();
+        chart.destroy(); // clear old information so it doesnt overflow
     }
-    chart = new Chart(document.getElementById("line-chart"), {
+    chart = new Chart(document.getElementById("line-chart"), { // create new chart structure for streaming data
         type: 'scatter',
         data: {},
         options: {
@@ -90,9 +90,9 @@ function addData(chart,times,data,types,color) {
                     }
                 }]
             },
-	    tooltips: {
+	    tooltips: { 
             	callbacks: {
-                    label: function (tti, data) {
+                    label: function (tti, data) { // callback to set tooltips
                         var lab = tti.yLabel + " (" + (new Date(tti.xLabel).toLocaleTimeString()) + ")";
                     	return lab;
                     }
@@ -101,12 +101,13 @@ function addData(chart,times,data,types,color) {
         }
     });
     lineChart = chart;
-    chart.options.title.text = 'Dataset Graph';
+    chart.options.title.text = 'Dataset Graph'; // graph title
     chart.data.labels = times;
-    for (x = 0;x<data.length;x++){
+
+    for (x = 0;x<data.length;x++){ // create plot for each set of data [elevation,pressure,etc...]
         var dataset = {
             label: types[x],
-            borderColor: getRandomColor(),
+            borderColor: getRandomColor(), // choose random color for now
             borderWidth: 3,
             fill: false,
             data: data[x],
